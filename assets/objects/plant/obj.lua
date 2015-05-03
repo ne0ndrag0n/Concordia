ef6d63602f2561bc624e47166280d6866241807a
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-05-03 13:14:09 -0400

    lua changes

--- assets/objects/plant/obj.lua
+++ assets/objects/plant/obj.lua
@@ -1,53 +1,36 @@
 -- This is the first BlueBear object to be tested!
 
+-- Objects must take the form of a table passed to bluebear.register_object with at least the following properties:
+-- init: This function is run when the object is first placed on the lot. Sets crucial attributes about the object
+-- main: This function is called when the object is due to be refreshed. It returns the amount to sleep for at least, in world seconds.
+-- The following are optional properties
+-- actions: If not nil, a table of actions taking the form { label, condition, action } where label is the pie menu label, condition is when the option appears, and action is the action taken
 local flowers = {
 
-	dead = false,
+	water_level = nil,
 
-	water_level = 100,
-
-	-- These properties are used by the lot engine (to add to its ambient light level or aesthetic level)
-	props = {
-		AESTHETIC_SCORE = 10		
-	},
-	
-	-- This property determines what will appear on the pie menu
-	pie_menu = bluebear.utility.extend(
-		bluebear.object.semiglobals.PLANT,
-		{ 'View', 'view_condition', 'view_action' },
-		{ 'Replant', 'replant_condition', 'replant_action' }
-	),
-	
-	-- This property handles actions when pie menu items are selected
 	actions = {
-		view_action = function( self, player, lot )
-			player.animate( bluebear.common.animations.OBSERVE, 5000 )
-			player.motives.aesthetic = player.motives.aesthetic + self.props.AESTHETIC_SCORE
-		end,
-		water_action = function( self, player, lot )
-			player.animate( bluebear.common.animations.WATER_PLANT, 3000 )
-			self.water_level = 100
-		end,
-		replant_action = function( self, player, lot ) 
-			player.animate( bluebear.common.animations.REPLANT, 10000 )
-			self.water_level = 1000
-			self.props.AESTHETIC_SCORE = 10
-			bluebear.set_object_image_state( "mipani_flowers", self.assets.image.FLOWERS_ALIVE )
-			self.dead = false
-		end
-	},
-	
-	-- This property determines WHEN items will appear on the pie menu
-	conditions = {
-		water_condition = function( self, player, lot )
-			return self.water_level >= 1
-		end,
-		
-		-- no view_condition - always true - always display pie menu item
-		
-		replant_condition = function( self, player, lot ) 
-			return self.dead
-		end
+		{
+			label = "Water",
+			condition = function( self, player, lot )
+				return self.water_level ~= 0
+			end,
+			action = function( self, player, lot ) 
+				print( "Watering" )
+				self.water_level = 100
+				print( "Watered" )
+			end
+		},
+		{
+			label = "Replant",
+			condition = function( self, player, lot ) 
+				return self.water_level == 0
+			end,
+			action = function( self, player, lot )
+				self.water_level = 100
+				print( "Replanted" )
+			end
+		}
 	},
 	
 	-- This function will run when the object is first placed on the lot
@@ -57,16 +40,8 @@ local flowers = {
 	
 	-- This function will run when the object is scheduled to update its status.
 	main = function( self, lot ) 
-		if self.water_level = 0 && self.dead == false
-			self.props.AESTHETIC_SCORE = -50
-			bluebear.set_object_image_state( "mipani_flowers", self.assets.image.FLOWERS_DEAD )
-			self.dead = true
-		elseif self.dead == false
+		if self.water_level > 0 
 			self.water_level = self.water_level - 10
-			-- Die quicker without enough ambient light
-			if lot.get_ambient_light() <= 30
-				self.water_level = self.water_level - 30
-			end
 		end
 		
 		-- Do not re-execute for at least 43200 game ticks (12 simulated game hours)
@@ -76,4 +51,4 @@ local flowers = {
 
 
 
-bluebear.register_object( "mipani_flowers", flowers )
+bluebear.register_object( flowers )
--- system/root.lua
+++ system/root.lua
@@ -1,27 +1,13 @@
 -- Provides the root environment for Lua scripts within the BlueBear engine
 -- DO NOT MODIFY THIS FILE
 
+_objcount = 0;
 _bbobjects = {};
 
 bluebear = {
 
-	object = {
-	
-		semiglobals = {
-			PLANT = {
-				WATER = { 'Water', 'water_condition', 'water_action' }
-			}
-		},
-		
-		register_object = function() 
-			-- todo
-		end
-	}
+	register_object = function() 
+		-- todo
+	end
 
-	utility = {
-		extend = function( t1, t2 )
-			--for k,v in ipairs( t2 ) do table.insert( t1, v ) end return t1
-			--concatenate tables if possible
-		end
-	}
 }
