7c45ce99e20c4340f57f24f05cfbe1498e1fb086
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-05-10 18:39:40 -0400

    Implement UML Design

--- include/pepe/bluebear.hpp
+++ include/pepe/bluebear.hpp
@@ -22,11 +22,54 @@ extern "C" {
 namespace BlueBear {
 
 	enum TerrainType { TERRAIN_GRASS, TERRAIN_DIRT, TERRAIN_SNOW, TERAIN_SAND, TERRAIN_MOON };
+	
+	class Object {
+		
+		private:
+			int luaVMInstance;
+			long nextExecution;
+		
+		public:
+			Object();
+			~Object();
+			void execute( long worldTicks );
+		
+	};
+	
+	class Floor {
+		
+		private:
+			std::vector< Object > objects;
+		
+		public:
+			Floor();
+			~Floor();
+			std::vector< Object > getObjects();
+		
+	};
+	
+	class Lot {
+		
+		private:
+			int floorX;
+			int floorY;
+			int undergroundStories;
+			std::vector< Floor > floors;
+			TerrainType terrainType;
+		
+		public:
+			Lot();
+			~Lot();
+			std::vector< Floor > getFloors();
+
+	};
 
 	class Engine {
 		
 		private:
 			lua_State* L;
+			long startTicks;
+			Lot* currentLot;
 
 		public:
 			Engine();
