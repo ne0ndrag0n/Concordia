adfcc7731ddbfc266b0f4bdd48f5a5efa5170249
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-05-27 18:02:17 -0400

    Lua deserialiser, untested

--- system/bblib.lua
+++ system/bblib.lua
@@ -1,7 +1,63 @@
 _bblib = {
 	serialise = function( table, content ) 
-		local table_count
+		-- TODO
+	end,
+	
+	deserialise = function( bytes ) 
+		local tbl = {}
+		local cursor = 1
 		
+		while true do
+			-- get character at cursor position, then advance cursor to key
+			local signal = bytes:sub( cursor, cursor )
+			cursor = cursor + 1
+			
+			-- if signal is \xFF, then break
+			if signal == "\xFF" then break end
+			
+			-- get key by starting at cursor, breaking out when we see a null terminator
+			local key = ""
+			
+			for cursor = cursor, #bytes do
+				local c = bytes:sub( cursor, cursor )
+				if c == "\x00" then break end
+				key = key..c
+			end
+			cursor = cursor + 1
+			
+			-- cursor should be pointed at data now
+			-- derive value
+			local value
+			if signal == "\x00" then
+				-- type boolean
+				value = bytes:sub( cursor, cursor ) 
+				value = ( value == '\x01' )
+				cursor = cursor + 1
+			elseif signal == "\x01" or signal == "\x02" then
+				-- type number or string
+				value = ""
+				for cursor = cursor, #bytes do
+					local c = bytes:sub( cursor, cursor )
+					if c == "\x00" then break end
+					value = value..c
+				end
+				cursor = cursor + 1
+				
+				-- if we got here from a number, convert it to number type. else, leave it go!
+				if signal == "\x01" then
+					value = tonumber( value )
+				end
+			elseif signal == "\x03" then
+				-- type table
+				-- nice, easy: repeat this entire process using a recursive call
+				value = _bblib.deserialise( bytes:sub( cursor, #bytes ) )
+			end
+			
+			-- set table key to value
+			tbl[key] = value
+			
+		end
 		
+		return tbl
 	end
 };
