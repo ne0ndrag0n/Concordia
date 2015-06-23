68ba962562906c8046364d6e9d689d6fec58f026
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-06-23 18:03:24 -0400

    Additional traits, another way of doing them

--- system/basetype.lua
+++ system/basetype.lua
@@ -8,6 +8,7 @@ local base_player = {
 			id = "hunger",
 			name = "Hunger",
 			decay_rate = 10,
+			decay_period = 6000,
 			events = {
 				{
 					trigger = "<= 0",
@@ -21,6 +22,7 @@ local base_player = {
 			id = "hygiene",
 			name = "Hygiene",
 			decay_rate = 10,
+			decay_period = 6000,
 			events = {
 				{
 					trigger = "<= 15",
@@ -35,14 +37,29 @@ local base_player = {
 			id = "bladder",
 			name = "Bladder",
 			decay_rate = 5,
+			decay_period = 6000,
 			increase = true,
 			events = {
-				trigger = "= 100",
-				callback = function( self, lot ) 
-					lot.spawn_object( "mipani.puddle", bluebear.Location.BELOW_PLAYER )
-					self.animate( self.Animations.EMBARASSED_1 )
-					self.set_motive( "hygiene", 15 )
-				end
+				{
+					trigger = "= 100",
+					callback = function( self, lot ) 
+						lot.spawn_object( "mipani.puddle", bluebear.Location.BELOW_PLAYER )
+						self.animate( self.Animations.EMBARASSED_1 )
+						self.set_motive( "hygiene", 15 )
+					end
+				}
+			}
+		},
+		{
+			id = "energy",
+			name = "Energy",
+			decay_rate = 5,
+			decay_period = 6000,
+			events = {
+				{
+					trigger = "<= 5",
+					callback = "sleep"
+				}
 			}
 		}
 	}
