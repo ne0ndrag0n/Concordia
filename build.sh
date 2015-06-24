afe0c88058b808ce02e5f8d8a25fbb5b9537b8a3
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-06-24 16:54:47 -0400

    Refactoring

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -g -rdynamic src/utility.cpp src/object.cpp src/lot.cpp src/engine.cpp src/main.cpp -Iinclude/lua -Iinclude/pepe -Llib -llua -ldl -o bluebear
+g++ -std=c++11 -g -rdynamic src/utility.cpp src/object.cpp src/lot.cpp src/engine.cpp src/main.cpp -Iinclude/lua -Iinclude/bluebear -Llib -llua -ldl -o bbexec
