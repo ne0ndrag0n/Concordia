807288b96590564b6e58be189bf13af77826b3dd
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2015-07-03 14:46:18 -0400

    Spacing fixes. ODT update (another oops). Next steps is rewrite Engine::verifyODT

--- system/bblib.lua
+++ system/bblib.lua
@@ -134,7 +134,7 @@ _bblib = {
 		return "bb"..self.lastcid
 	end,
 	
-	instantiate_pop = function( id, pop, entity_type )	
+	instantiate_pop = function( id, pop, entity_type )
 		local bbobject = _classes[ bluebear.Constants.LOT_ENTITY_TYPES[ entity_type ] ][ id ].new()
 		bbobject.type = id 
 		bbobject._cid = _bblib:get_cid()
