3be667343aa67423cbd143e96e74e3245d7c9812
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-04-17 07:37:48 -0400

    Cleanup

--- include/bluebear/utility.hpp
+++ include/bluebear/utility.hpp
@@ -33,36 +33,19 @@ namespace BlueBear {
 		void clearLuaStack( lua_State* L );
 
 		void getTableValue( lua_State* L, const char* key );
-		
+
 		void setTableIntValue( lua_State* L, const char* key, int value );
-		
+
 		void setTableStringValue( lua_State* L, const char* key, const char* value );
-		
+
 		void setTableFunctionValue( lua_State* L, const char* key, lua_CFunction value );
-		
+
 		void doDirectories( lua_State* L, const char* directory );
-		
+
 		void strace();
-		
-		inline uint8_t getuint8_t( std::ifstream* istream ) {
-			uint8_t num;
-			istream->read( reinterpret_cast< char* >( &num ), 1 );
-			return num;
-		};
-		
-		inline uint16_t getuint16_t( std::ifstream* istream ) {
-			uint16_t num;
-			istream->read( reinterpret_cast< char* >( &num ), 2 );
-			return Utility::swap_uint16( num );
-		};
-		
-		inline uint32_t getuint32_t( std::ifstream* istream ) {
-			uint32_t num;
-			istream->read( reinterpret_cast< char* >( &num ), 4 );
-			return Utility::swap_uint32( num );
-		};
+
 	};
 }
 
 
-#endif 
+#endif
