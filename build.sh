6ca88ad1a5cd067fe8283101172873b7b1c4270f
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-04-24 11:37:41 -0400

    Reorg of native code headers

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -g -rdynamic include/lib/jsoncpp.cpp src/utility.cpp src/lotentity.cpp src/lot.cpp src/engine.cpp src/main.cpp -Iinclude/lib -Iinclude/lib/json -Iinclude/lua -Iinclude/bluebear -Llib -llua -ldl -o bbexec
+g++ -std=c++11 -g -rdynamic include/json/jsoncpp.cpp src/utility.cpp src/lotentity.cpp src/lot.cpp src/engine.cpp src/main.cpp -Iinclude/json -Iinclude/json/json -Iinclude/lua -Iinclude/bluebear -Llib -llua -ldl -o bbexec
