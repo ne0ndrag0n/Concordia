5fa53cfe4f1c53ed21d0069c92955e12a031e161
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-04-30 14:18:33 -0400

    System motive object

--- system/motive/obj.lua
+++ system/motive/obj.lua
@@ -3,8 +3,39 @@
   basic motives.
 --]]
 
-bluebear.motives = {}
+bluebear.engine.require_modpack( "yaci" )
 
-bluebear.register_motive = function( motiveName, motiveObject )
-  bluebear.motives[ motiveName ] = motiveObject
+local Motive = newclass()
+
+Motive.name = "Root Motive"
+
+function Motive:init( doll )
+  self.doll = doll
+  self.value = 50
 end
+
+function Motive:decay()
+  if self.value > 0 then
+    self.value = self.value - 1
+  end
+end
+
+function Motive:increment_by( value )
+  self:set_value( self.value + value )
+end
+
+function Motive:decrement_by( value )
+  self:set_value( self.value - value )
+end
+
+function Motive:set_value( value )
+  if value > 100 then
+    self.value = 100
+  elseif value < 0 then
+    self.value = 0
+  else
+    self.value = value
+  end
+end
+
+bluebear.register_class( "system.motive.base", Motive )
