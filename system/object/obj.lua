924f84116de7792b0419d1b3618763a5e4e09c97
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-05-13 19:24:36 -0400

    #44 Fix issue where LotEntity had the rug pulled out from under it

--- system/object/obj.lua
+++ system/object/obj.lua
@@ -14,7 +14,7 @@ function Object:load( saved )
 
 	self._cid = data._cid
 
-	if type( self._cid ) == 'number' then
+	if type( self._cid ) == 'string' then
 		-- Every object needs to track cid on bluebear.util.lastcid
 		-- when deserialising: the largest one we find is the new lastcid!
 		local numericCid = tonumber( string.match( self._cid or "", "%d+" ) )
