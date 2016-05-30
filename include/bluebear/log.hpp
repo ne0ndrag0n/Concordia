da03b835e33ea6acf330dfff5eb0b66d8eadc1a2
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-05-30 14:11:12 -0400

    #17 some random getting started in Log

--- include/bluebear/log.hpp
+++ include/bluebear/log.hpp
@@ -13,19 +13,25 @@ namespace BlueBear {
   class Log {
 
     private:
+      enum class LogMode { CONSOLE, FILE, BOTH };
       enum class LogLevel { DEBUG, INFO, WARN, ERROR };
 
-      class LogMessage {
+      struct LogMessage {
         std::string tag;
         std::string message;
         LogLevel level;
       };
 
       std::vector< LogMessage > messages;
+      LogLevel minimumReportableLevel;
+      LogMode mode;
 
       Log();
       Log( Log const& );
       void operator=( Log const& );
+      void out( const LogMessage& message );
+      void outToConsole( const LogMessage& message );
+      void outToFile( const LogMessage& message );
 
     public:
       static Log& getInstance() {
@@ -33,10 +39,10 @@ namespace BlueBear {
         return instance;
       }
 
-      void debug( std::string tag, std::string message );
-      void info( std::string tag, std::string message );
-      void warn( std::string tag, std::string message );
-      void error( std::string tag, std::string message );
+      void debug( const std::string& tag, const std::string& message );
+      void info( const std::string& tag, const std::string& message );
+      void warn( const std::string& tag, const std::string& message );
+      void error( const std::string& tag, const std::string& message );
   };
 }
 
--- src/log.cpp
+++ src/log.cpp
@@ -1,1 +1,35 @@
 #include "log.hpp"
+#include <string>
+
+namespace BlueBear {
+
+  void Log::out( const LogMessage& message ) {
+    messages.push_back( message );
+
+    // sucks
+    if( mode == LogMode::CONSOLE || mode == LogMode::BOTH ) {
+      outToConsole( message );
+    }
+    if( mode == LogMode::FILE || mode == LogMode::BOTH ) {
+      outToFile( message );
+    }
+  }
+
+  void Log::outToConsole( const LogMessage& message ) {
+    #ifdef _WIN32
+      std::cout << "[" << message.tag << "] " << message.message << std::endl;
+    #else
+      std::cout << "[" << message.tag << "] " << message.message << std::endl;
+    #endif
+  }
+
+  void Log::outToFile( const LogMessage& message ) {
+    // STUB !!
+  }
+
+  void Log::debug( const std::string& tag, const std::string& message ) {
+    out( LogMessage { tag, message, LogLevel::DEBUG } );
+  }
+
+
+}
