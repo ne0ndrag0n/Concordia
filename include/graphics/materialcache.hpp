aca43cb6db8bb81cb3c02bbe53c81ad8838af3a0
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-08-17 16:53:12 -0400

    MaterialCache headers

--- include/graphics/materialcache.hpp
+++ include/graphics/materialcache.hpp
@@ -1,12 +1,26 @@
 #ifndef MATERIALCACHE
 #define MATERIALCACHE
 
+#include "graphics/texturecache.hpp"
+#include <string>
+#include <map>
+#include <memory>
 
 namespace BlueBear {
+  namespace Scripting {
+    class Tile;
+  }
+
   namespace Graphics {
+    class Material;
 
     class MaterialCache {
+      TextureCache textureCache;
+      std::map< std::string, std::shared_ptr< Material > > materialCache;
 
+      public:
+        std::shared_ptr< Material > get( Scripting::Tile& tile );
+        void prune();
     };
   }
 }
