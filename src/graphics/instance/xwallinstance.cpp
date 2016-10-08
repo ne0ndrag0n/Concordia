85d8473977214f56bedd609bd19cbfbbdf8515ff
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-10-08 08:38:12 -0400

    Segments for X

--- src/graphics/instance/xwallinstance.cpp
+++ src/graphics/instance/xwallinstance.cpp
@@ -18,7 +18,20 @@ namespace BlueBear {
     XWallInstance::XWallInstance( GLuint shaderProgram, TextureCache& hostTextureCache, ImageCache& hostImageCache ) : WallInstance::WallInstance( *XWallInstance::Piece, shaderProgram, hostTextureCache, hostImageCache ) {}
 
     void XWallInstance::setRotationAttributes( unsigned int rotation, std::map< std::string, std::unique_ptr< ImageSource > >& settings ) {
-
+      switch( rotation ) {
+        case 0:
+          settings.emplace( std::make_pair( "Side2", std::make_unique< DirectImageSource >( *back.rightSegment, "0xs2 " + back.path ) ) );
+          break;
+        case 1:
+          settings.emplace( std::make_pair( "Side1", std::make_unique< DirectImageSource >( *back.leftSegment, "1xs1 " + back.path ) ) );
+          break;
+        case 2:
+          settings.emplace( std::make_pair( "Side1", std::make_unique< DirectImageSource >( *front.rightSegment, "2xs1 " + front.path ) ) );
+          break;
+        case 3:
+        default:
+          settings.emplace( std::make_pair( "Side2", std::make_unique< DirectImageSource >( *front.leftSegment, "3xs2 " + front.path ) ) );
+      }
     }
 
   }
