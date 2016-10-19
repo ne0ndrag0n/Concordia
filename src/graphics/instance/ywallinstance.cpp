98c21a7c89efb60284b6fed713287725f13eae7b
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-10-19 15:33:07 -0400

    First setRotationAttributesEdge

--- src/graphics/instance/ywallinstance.cpp
+++ src/graphics/instance/ywallinstance.cpp
@@ -18,9 +18,12 @@ namespace BlueBear {
 
     YWallInstance::YWallInstance( GLuint shaderProgram, TextureCache& hostTextureCache, ImageCache& hostImageCache, bool edgePiece ) :
       WallInstance::WallInstance( edgePiece ? *YWallInstance::EdgePiece : *YWallInstance::Piece, shaderProgram, hostTextureCache, hostImageCache ) {
-        selectedRotationFunction = std::bind( &YWallInstance::setRotationAttributes, this, std::placeholders::_1, std::placeholders::_2 );
+        selectedRotationFunction = std::bind( edgePiece ? &YWallInstance::setRotationAttributesEdge : &YWallInstance::setRotationAttributes, this, std::placeholders::_1, std::placeholders::_2 );
     }
 
+    /**
+     * Set the texture atlas attributes per rotation
+     */
     void YWallInstance::setRotationAttributes( unsigned int rotation, std::map< std::string, std::unique_ptr< ImageSource > >& settings ) {
       switch( rotation ) {
         case 0:
@@ -35,6 +38,26 @@ namespace BlueBear {
         case 3:
         default:
           settings.emplace( std::make_pair( "Side1", std::make_unique< DirectImageSource >( *front.rightSegment, "3ys1 " + front.path ) ) );
+      }
+    }
+
+    void YWallInstance::setRotationAttributesEdge( unsigned int rotation, std::map< std::string, std::unique_ptr< ImageSource > >& settings ) {
+      YWallInstance::setRotationAttributes( rotation, settings );
+
+      // Apply additional settings to each extended attribute for this extended wall instance
+      switch( rotation ) {
+        case 0:
+          settings.emplace( std::make_pair( "FrontWallExtended", std::make_unique< DirectImageSource >( *front.rightSegment, "0yf " + front.path ) ) );
+          break;
+        case 1:
+          settings.emplace( std::make_pair( "BackWallExtended", std::make_unique< DirectImageSource >( *back.leftSegment, "1yb " + back.path ) ) );
+          break;
+        case 2:
+          settings.emplace( std::make_pair( "BackWallExtended", std::make_unique< DirectImageSource >( *back.rightSegment, "2yb " + back.path ) ) );
+          break;
+        case 3:
+        default:
+          settings.emplace( std::make_pair( "FrontWallExtended", std::make_unique< DirectImageSource >( *front.leftSegment, "3yf " + front.path ) ) );
       }
     }
 
