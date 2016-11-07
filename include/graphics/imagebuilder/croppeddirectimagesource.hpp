ccfc63649355f45e5f4e806117dc2010aedaf678
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2016-11-07 18:46:20 -0500

    Publicise members of CroppedDirectImageSource for composition over inheritance

--- include/graphics/imagebuilder/croppeddirectimagesource.hpp
+++ include/graphics/imagebuilder/croppeddirectimagesource.hpp
@@ -9,11 +9,11 @@ namespace BlueBear {
   namespace Graphics {
 
     class CroppedDirectImageSource : public DirectImageSource {
+    public:
       int x;
       int y;
       int w;
       int h;
-    public:
       CroppedDirectImageSource( sf::Image& image, int x, int y, int w, int h, const std::string& key );
       sf::Image getImage();
       std::string getKey();
