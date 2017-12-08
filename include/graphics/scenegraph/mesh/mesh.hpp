8dd9165095a1cd91ad9ab73b8cb61854c4020af5
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-12-08 18:46:12 -0500

    Remove StorageMethod; don't need it.

--- include/graphics/scenegraph/mesh/mesh.hpp
+++ include/graphics/scenegraph/mesh/mesh.hpp
@@ -6,31 +6,42 @@
 #include <vector>
 #include <memory>
 #include <map>
+#include <functional>
 
 namespace BlueBear {
   namespace Graphics {
     namespace SceneGraph {
       namespace Mesh {
 
-        template < typename VertexType, typename StorageMethod > class Mesh {
-          using Index = GLuint;
-
+        template < typename VertexType > class Mesh {
           GLuint VAO;
           GLuint VBO;
           GLuint EBO;
           unsigned int size;
+          bool indexed;
+
+          std::function< void() > drawMethod;
 
           // Disable copy
           Mesh( const Mesh& );
           Mesh& operator=( const Mesh& );
 
+          void drawIndexed() {
+            glDrawElements( GL_TRIANGLES, size, GL_UNSIGNED_INT, 0 );
+          }
+
+          void drawVertices() {
+            // TODO: forgot how to draw triangles without EBO
+          }
+
         public:
           std::map< std::string, std::unique_ptr< MeshUniform > > meshUniforms;
 
-          Mesh( const std::vector< VertexType >& vertices, const std::vector< GLuint >& indices ) {
+          Mesh( const std::vector< VertexType >& vertices, const std::vector< GLuint >& indices ) :
+            size( indices.size() ), indexed( true ), drawMethod( &Mesh::drawIndexed ) {
             glGenVertexArrays( 1, &VAO );
             glGenBuffers( 1, &VBO );
-            StorageMethod::allocateIndices( &EBO );
+            glGenBuffers( 1, &EBO );
 
             glBindVertexArray( VAO );
               glBindBuffer( GL_ARRAY_BUFFER, VBO );
@@ -38,7 +49,23 @@ namespace BlueBear {
                 glBufferData( GL_ARRAY_BUFFER, vertices.size() * sizeof( VertexType ), &vertices[ 0 ], GL_STATIC_DRAW );
                 VertexType::setupLayout();
 
-                StorageMethod::uploadIndices( EBO, indices );
+                glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+                glBufferData( GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof( GLuint ), &indices[ 0 ], GL_STATIC_DRAW );
+
+              glBindBuffer( GL_ARRAY_BUFFER, 0 );
+            glBindVertexArray( 0 );
+          }
+
+          Mesh( const std::vector< VertexType >& vertices ) :
+            size( vertices.size() ), indexed( false ), drawMethod( &Mesh::drawVertices ) {
+            glGenVertexArrays( 1, &VAO );
+            glGenBuffers( 1, &VBO );
+
+            glBindVertexArray( VAO );
+              glBindBuffer( GL_ARRAY_BUFFER, VBO );
+
+                glBufferData( GL_ARRAY_BUFFER, vertices.size() * sizeof( VertexType ), &vertices[ 0 ], GL_STATIC_DRAW );
+                VertexType::setupLayout();
 
               glBindBuffer( GL_ARRAY_BUFFER, 0 );
             glBindVertexArray( 0 );
@@ -47,7 +74,9 @@ namespace BlueBear {
           ~Mesh() {
             glDeleteVertexArrays( 1, &VAO );
             glDeleteBuffers( 1, &VBO );
-            StorageMethod::deallocateIndices( &EBO );
+            if( indexed ) {
+              glDeleteBuffers( 1, &EBO );
+            }
           }
 
           void drawElements() {
@@ -56,7 +85,7 @@ namespace BlueBear {
             }
 
             glBindVertexArray( VAO );
-              StorageMethod::draw( size );
+              drawMethod();
             glBindVertexArray( 0 );
           }
 
