353ec81e5a11287d918c53dff8f44ad68800b58a
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-12-26 21:50:28 -0500

    OpenGL::lock to guard concurrent access of OpenGL

--- include/tools/opengl.hpp
+++ include/tools/opengl.hpp
@@ -1,26 +1,25 @@
 #ifndef OPENGL_TOOLS
 #define OPENGL_TOOLS
 
+#include "exceptions/genexc.hpp"
 #include <GL/glew.h>
 #include <string>
 #include <exception>
+#include <functional>
+#include <mutex>
 
 namespace BlueBear {
   namespace Tools {
 
     class OpenGL {
+      static std::mutex mutex;
+
     public:
+      EXCEPTION_TYPE( IncorrectShaderException, "Applied uniform to incorrect shader!" );
+
       static GLint getUniformLocation( const std::string& uniform );
       static GLint getUniformID( const std::string& uniform );
-
-      struct IncorrectShaderException : public std::exception {
-
-        const char* what() const throw() {
-          return "Applied uniform to incorrect shader!";
-        }
-
-      };
-
+      static void lock( std::function< void() > predicate );
     };
 
   }
--- src/tools/opengl.cpp
+++ src/tools/opengl.cpp
@@ -4,6 +4,8 @@
 namespace BlueBear {
   namespace Tools {
 
+    std::mutex OpenGL::mutex;
+
     GLint OpenGL::getUniformID( const std::string& uniform ) {
       return glGetUniformLocation( Graphics::Shader::CURRENT_PROGRAM, uniform.c_str() );
     }
@@ -15,6 +17,16 @@ namespace BlueBear {
       }
 
       return uniformLocation;
+    }
+
+    /**
+     * Use this to wrap any OpenGL code that may be run concurrently. OpenGL is completely thread unsafe,
+     * and things like loading models while the game loop continues may require locks on anything touching
+     * OpenGL.
+     */
+    void OpenGL::lock( std::function< void() > predicate ) {
+      std::lock_guard< std::mutex > guard( OpenGL::mutex );
+      predicate();
     }
 
   }
