56b9ed74d91ffecfe1b116d64d2b7ad599bef980
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-01-22 16:29:41 -0500

    Refactor renderer into component

---
+++ include/device/display/adapter/component/renderer.hpp
@@ -0,0 +1,61 @@
+#ifndef WORLD_RENDERER
+#define WORLD_RENDERER
+
+#include "containers/vec3map.hpp"
+#include "graphics/camera.hpp"
+#include "graphics/scenegraph/resourcebank.hpp"
+#include <tbb/concurrent_unordered_map.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+
+namespace BlueBear {
+  namespace Graphics {
+    class Shader;
+
+    namespace SceneGraph {
+      class Model;
+      class Light;
+
+      namespace ModelLoader {
+        class FileModelLoader;
+      }
+    }
+  }
+
+  namespace Device {
+    namespace Display {
+      namespace Adapter {
+        namespace Component {
+
+          class Renderer {
+            Graphics::Camera camera;
+
+            Graphics::SceneGraph::ResourceBank cache;
+            tbb::concurrent_unordered_map< std::string, std::shared_ptr< Graphics::SceneGraph::Model > > originals;
+
+            // TODO: Walls and floor which both consist of one generated model
+            std::unordered_map< std::string, std::shared_ptr< Graphics::SceneGraph::Light > > lights;
+            std::unordered_map< std::string, std::shared_ptr< Graphics::SceneGraph::Model > > models;
+
+            std::unique_ptr< Graphics::SceneGraph::ModelLoader::FileModelLoader > getFileModelLoader( bool deferGLOperations );
+
+          public:
+            Renderer();
+            virtual ~Renderer() = default;
+
+            void loadPathsParallel( const std::vector< std::pair< std::string, std::string > >& paths );
+            void loadPaths( const std::vector< std::pair< std::string, std::string > >& paths );
+            void render();
+          };
+
+        }
+      }
+    }
+  }
+
+}
+
+#endif
---
+++ src/device/display/adapter/component/renderer.cpp
@@ -0,0 +1,96 @@
+#include "device/display/adapter/component/renderer.hpp"
+#include "graphics/scenegraph/model.hpp"
+#include "graphics/scenegraph/modelloader/filemodelloader.hpp"
+#include "graphics/scenegraph/modelloader/assimpmodelloader.hpp"
+#include "tools/objectpool.hpp"
+#include "tools/utility.hpp"
+#include "configmanager.hpp"
+#include "log.hpp"
+#include <tbb/task_group.h>
+#include <tbb/concurrent_queue.h>
+#include <functional>
+
+namespace BlueBear {
+  namespace Device {
+    namespace Display {
+      namespace Adapter {
+        namespace Component {
+
+          Renderer::Renderer() : camera( Graphics::Camera( ConfigManager::getInstance().getIntValue( "viewport_x" ), ConfigManager::getInstance().getIntValue( "viewport_y" ) ) ) {}
+
+          std::unique_ptr< Graphics::SceneGraph::ModelLoader::FileModelLoader > Renderer::getFileModelLoader( bool deferGLOperations ) {
+            std::unique_ptr< Graphics::SceneGraph::ModelLoader::FileModelLoader > result = std::make_unique< Graphics::SceneGraph::ModelLoader::AssimpModelLoader >();
+
+            Graphics::SceneGraph::ModelLoader::AssimpModelLoader& asAssimp = ( Graphics::SceneGraph::ModelLoader::AssimpModelLoader& )*result;
+            asAssimp.deferGLOperations = true;
+            asAssimp.cache = &cache;
+
+            return result;
+          }
+
+          void Renderer::loadPathsParallel( const std::vector< std::pair< std::string, std::string > >& paths ) {
+            tbb::task_group group;
+            Tools::ObjectPool< Graphics::SceneGraph::ModelLoader::FileModelLoader > pool( std::bind( &Renderer::getFileModelLoader, this, true ) );
+
+            for( auto& path : paths ) {
+              group.run( [ & ]() {
+                if( originals.find( path.first ) == originals.end() ) {
+                  pool.acquire( [ & ]( Graphics::SceneGraph::ModelLoader::FileModelLoader& loader ) {
+                    try {
+                      if( std::shared_ptr< Graphics::SceneGraph::Model > model = loader.get( path.second ) ) {
+                        originals[ path.first ] = model;
+                      }
+                    } catch( std::exception& e ) {
+                      Log::getInstance().error( "Renderer::loadPathsParallel", std::string( "Could not load model " ) + path.second + ": " + e.what() );
+                    }
+                  } );
+                } else {
+                  Log::getInstance().warn( "Renderer::loadPathsParallel", path.first + " is already inserted into this map; skipping" );
+                }
+              } );
+            }
+
+            group.wait();
+
+            for( auto& path : paths ) {
+              auto pair = originals.find( path.first );
+              if( pair != originals.end() ) {
+                pair->second->sendDeferredObjects();
+              }
+            }
+          }
+
+          void Renderer::loadPaths( const std::vector< std::pair< std::string, std::string > >& paths ) {
+            std::unique_ptr< Graphics::SceneGraph::ModelLoader::FileModelLoader > loader = getFileModelLoader( false );
+
+            for( auto& path : paths ) {
+              try {
+                if( std::shared_ptr< Graphics::SceneGraph::Model > model = loader->get( path.second ) ) {
+                  originals[ path.first ] = model;
+                }
+              } catch( std::exception& e ) {
+                Log::getInstance().error( "Renderer::loadPathsParallel", std::string( "Could not load model " ) + path.second + ": " + e.what() );
+              }
+            }
+          }
+
+          /**
+           * TODO: Optimized renderer that sorts by shader to minimize shader changes
+           */
+          void Renderer::render() {
+            // Position camera
+            camera.position();
+
+            // TODO: Position lights
+            // TODO: Draw floor and walls
+            // Draw models
+            for( auto& pair : models ) {
+              pair.second->draw();
+            }
+          }
+
+        }
+      }
+    }
+  }
+}
