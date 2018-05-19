4744bb18e7cf779de2265842616188e608d422aa
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-05-19 17:29:55 -0400

    Performance regression fixes

--- src/graphics/userinterface/widgets/input.cpp
+++ src/graphics/userinterface/widgets/input.cpp
@@ -212,8 +212,14 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
   }
 
   void Input::reflow() {
-    if( auto parent = getParent() ) {
-      parent->reflow();
+    // Do not ask parent to reflow if width/height were not changed
+    std::unordered_set< std::string > changedInFrame = localStyle.getChangedAttributes();
+    if( changedInFrame.count( "width" ) || changedInFrame.count( "height" ) ) {
+      if( auto parent = getParent() ) {
+        parent->reflow();
+      } else {
+        Element::reflow();
+      }
     } else {
       Element::reflow();
     }
