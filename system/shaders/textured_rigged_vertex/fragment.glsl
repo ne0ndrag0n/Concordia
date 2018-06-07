085a91e0250bc291d3633673548a02c418db3160
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-06-07 16:08:11 -0400

    Better lighting

--- system/shaders/textured_rigged_vertex/fragment.glsl
+++ system/shaders/textured_rigged_vertex/fragment.glsl
@@ -27,17 +27,18 @@ void main() {
   vec3 result = vec3( 0.0, 0.0, 0.0 );
 
   for( uint i = 0u; i != numLights; i++ ) {
-    vec3 ambient = lights[ i ].ambient * material.ambient;
+    vec3 texResult = texture( material.diffuse0, fragTexture ).xyz;
+    vec3 ambient = lights[ i ].ambient * texResult;
 
     vec3 normal = normalize( fragNormal );
     vec3 lightDirection = normalize( lights[ i ].position - fragPos );
     float theta = max( dot( normal, lightDirection ), 0.0 );
-    vec3 diffuse = lights[ i ].diffuse * ( theta * texture( material.diffuse0, fragTexture ).xyz );
+    vec3 diffuse = lights[ i ].diffuse * ( theta * texResult );
 
     vec3 viewDirection = normalize( cameraPos - fragPos );
     vec3 reflectDirection = reflect( -lightDirection, normal );
     float spec = pow( max( dot( viewDirection, reflectDirection ), 0.0 ), material.shininess );
-    vec3 specular = lights[ i ].specular * ( spec * material.specular );
+    vec3 specular = lights[ i ].specular * ( spec * texResult );
 
     result += ambient + diffuse + specular;
   }
