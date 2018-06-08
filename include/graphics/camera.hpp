73fa7316747ae1cd920a2eaf6e37fa9e981c472e
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-06-08 16:47:26 -0400

    Pare down and repair camera

--- include/graphics/camera.hpp
+++ include/graphics/camera.hpp
@@ -17,16 +17,9 @@ namespace BlueBear {
         glm::mat4 view;
         glm::mat4 projection;
 
-        GLfloat cameraHeight = 3.0f;
-        glm::vec3 lookingAt = glm::vec3( 0.0f, 0.0f, -9.0f );
-        glm::vec3 camera = glm::vec3( 0, 0, -10.0f );
-        glm::vec3 originalDirection = glm::vec3( glm::normalize( lookingAt - camera ) );
-        glm::vec3 up = glm::vec3( 0.0f, 0.0f, 1.0f );
-        GLfloat yaw = 0.0f;
-        GLfloat pitch = 0.0f;
+        glm::vec3 camera = glm::vec3( 0, 0, 10.0f );
         GLuint currentRotation = 0;
 
-        float perspectiveAspectRatio;
         float widthHalf;
         float heightHalf;
         float zoom = 1.0f;
@@ -38,11 +31,8 @@ namespace BlueBear {
         void doRotate();
 
       public:
-        glm::vec3 direction = originalDirection;
-        bool ortho = true;
         Camera( int screenWidth, int screenHeight );
         void move( GLfloat x, GLfloat y, GLfloat z );
-        bool setOrthographic( bool flag );
         float zoomIn();
         float zoomOut();
         float setZoom( float zoomSetting );
@@ -50,15 +40,10 @@ namespace BlueBear {
         void sendToShader();
         glm::mat4 getOrthoView();
         glm::mat4 getOrthoMatrix();
-        void walkForward();
-        void walkBackward();
-        void updateFirstPersonView( int deltaX, int deltaY );
         unsigned int rotateRight();
         unsigned int rotateLeft();
         GLuint getCurrentRotation();
         void setRotationDirect( GLuint rotation );
-        std::string positionToString();
-        std::string directionToString();
     };
   }
 }
--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -18,7 +18,6 @@ namespace BlueBear {
     Camera::Camera( int screenWidth, int screenHeight ) {
       widthHalf = ( (float)screenWidth / 2 );
       heightHalf = ( (float)screenHeight / 2 );
-      perspectiveAspectRatio = (float)screenWidth / (float)screenHeight;
 
       eventManager.SHADER_CHANGE.listen( this, std::bind( &Camera::sendToShader, this ) );
     }
@@ -50,19 +49,6 @@ namespace BlueBear {
       dirty = true;
     }
 
-    bool Camera::setOrthographic( bool flag ) {
-      ortho = flag;
-
-      if( ortho ) {
-        camera = glm::vec3( 0.0f, 0.0f, -10.0f );
-      } else {
-        camera = glm::vec3( -cameraHeight, -cameraHeight, lookingAt.z + cameraHeight );
-      }
-
-      dirty = true;
-      return ortho;
-    }
-
     float Camera::zoomIn() {
       if( zoom != 1.0f ) {
         zoom -= zoomIncrement;
@@ -90,9 +76,7 @@ namespace BlueBear {
       if( dirty ) {
         // getOrthoView for now
         view = getOrthoView();
-        projection = ortho ?
-           getOrthoMatrix() :
-           glm::perspective( 45.0f, perspectiveAspectRatio, 0.1f, 50.0f );
+        projection = getOrthoMatrix();
 
         dirty = false;
       }
@@ -128,61 +112,15 @@ namespace BlueBear {
       return ortho;
     }
 
-    void Camera::walkForward() {
-      camera += glm::vec3( direction.x * 0.1f, direction.y * 0.1f, 0.0f );
-      dirty = true;
-    }
-
-    void Camera::walkBackward() {
-      camera -= glm::vec3( direction.x * 0.1f, direction.y * 0.1f, 0.0f );
-      dirty = true;
-    }
-
-    void Camera::updateFirstPersonView( int deltaX, int deltaY ) {
-      if( !( deltaX == 0 && deltaY == 0 ) ) {
-        // These are the correct settings for an up normal of Y, not Z
-        // Adjust them to work for an up normal of Z...
-        GLfloat xOffset = -deltaX * 0.07f;
-        GLfloat yOffset = -deltaY * 0.07f;
-
-        // Flip because our world is ass-backwards
-        yaw += xOffset;
-        pitch += yOffset;
-
-        if( pitch > 89.0f ) {
-          pitch = 89.0f;
-        }
-        if( pitch < -89.0f ) {
-          pitch = -89.0f;
-        }
-
-        glm::vec3 newDirection(
-          cos( glm::radians( yaw ) ) * cos( glm::radians( pitch ) ),
-          sin( glm::radians( yaw ) ) * cos( glm::radians( pitch ) ),
-          sin( glm::radians( pitch ) )
-        );
-        direction = glm::normalize( newDirection );
-        dirty = true;
-      }
-    }
-
     unsigned int Camera::rotateRight() {
-      if( ortho ) {
-        currentRotation = ( currentRotation + 1 ) % 4;
-
-        doRotate();
-      }
-
+      currentRotation = ( currentRotation + 1 ) % 4;
+      doRotate();
       return currentRotation;
     }
 
     unsigned int Camera::rotateLeft() {
-      if( ortho ) {
-        currentRotation = ( currentRotation - 1 ) % 4;
-
-        doRotate();
-      }
-
+      currentRotation = ( currentRotation - 1 ) % 4;
+      doRotate();
       return currentRotation;
     }
 
@@ -191,31 +129,14 @@ namespace BlueBear {
     }
 
     void Camera::setRotationDirect( GLuint rotation ) {
-      if( ortho ) {
-
-        if( rotation > 3 ) {
-          Log::getInstance().warn( "Camera::setRotationDirect", "Orthographic rotation not in interval [0,3] (value provided was " + std::to_string( rotation ) + "). Coercing to value in this range..." );
-          rotation = rotation % 4;
-        }
-
-        currentRotation = rotation;
-
-        doRotate();
+      if( rotation > 3 ) {
+        Log::getInstance().warn( "Camera::setRotationDirect", "Orthographic rotation not in interval [0,3] (value provided was " + std::to_string( rotation ) + "). Coercing to value in this range..." );
+        rotation = rotation % 4;
       }
-    }
 
-    std::string Camera::positionToString() {
-      // Yet another GCC bug (surprise, surprise): the identifier "CAMERA" is not usable here because it's confusing it for either
-      // "Camera" or "camera", two things that are not any of the other items. GJ GCC!
-      static std::string CAMERA_STRING( LocaleManager::getInstance().getString( "CAMERA" ) );
+      currentRotation = rotation;
 
-      return CAMERA_STRING + ": " + std::to_string( camera.x ) + ", " + std::to_string( camera.y ) + ", " + std::to_string( camera.z );
-    }
-
-    std::string Camera::directionToString() {
-      static std::string DIRECTION( LocaleManager::getInstance().getString( "DIRECTION" ) );
-
-      return DIRECTION + ": " + std::to_string( direction.x ) + ", " + std::to_string( direction.y ) + ", " + std::to_string( direction.z );;
+      doRotate();
     }
 
   }
