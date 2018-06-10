6a06843ecf997d696ed79015745386a6a403a986
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-06-10 10:29:53 -0400

    Update miscellaneous shaders

--- system/shaders/textured_vertex/fragment.glsl
+++ system/shaders/textured_vertex/fragment.glsl
@@ -5,15 +5,42 @@ in vec3 fragPos;
 out vec4 color;
 
 struct Material {
-  vec3 ambient;
   sampler2D diffuse0;
-  vec3 specular;
   float shininess;
+  float opacity;
+};
+
+struct Light {
+  vec3 position;
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
 };
 
 uniform vec3 cameraPos;
 uniform Material material;
+uniform uint numLights;
+uniform Light lights[ 16 ];
 
 void main() {
-  color = texture( material.diffuse0, fragTexture );
+  vec3 texResult = texture( material.diffuse0, fragTexture ).xyz;
+  vec3 result = vec3( 0.0, 0.0, 0.0 );
+
+  for( uint i = 0u; i != numLights; i++ ) {
+    vec3 ambient = lights[ i ].ambient * texResult;
+
+    vec3 normal = normalize( fragNormal );
+    vec3 lightDirection = normalize( lights[ i ].position - fragPos );
+    float theta = max( dot( normal, lightDirection ), 0.0 );
+    vec3 diffuse = lights[ i ].diffuse * ( theta * texResult );
+
+    vec3 viewDirection = normalize( cameraPos - fragPos );
+    vec3 reflectDirection = reflect( -lightDirection, normal );
+    float spec = pow( max( dot( viewDirection, reflectDirection ), 0.0 ), material.shininess );
+    vec3 specular = lights[ i ].specular * ( spec * texResult );
+
+    result += ambient + diffuse + specular;
+  }
+
+  color = vec4( result, material.opacity );
 }
--- system/shaders/textured_vertex/vertex.glsl
+++ system/shaders/textured_vertex/vertex.glsl
@@ -1,6 +1,6 @@
 #version 330 core
-layout (location = 0) in vec3 position; // The position variable has attribute position 0
-layout (location = 1) in vec3 normal; // This is currently unused
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 texture;
 
 out vec2 fragTexture;
@@ -12,10 +12,8 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-  mat4 mvp = projection * view * model;
-
-  gl_Position = mvp * vec4( position, 1.0f );
+  gl_Position = projection * view * model * vec4( position, 1.0f );
   fragTexture = texture;
-  fragNormal = normal;
+  fragNormal = mat3( transpose( inverse( model ) ) ) * normal;
   fragPos = vec3( model * vec4( position, 1.0 ) );
 }
