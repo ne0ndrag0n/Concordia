1a0314e75d3a5e97d3d6a6967330aa9d92ad56ca
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2018-06-17 12:31:51 -0400

    Getting started with FloorModelLoader

--- include/graphics/scenegraph/modelloader/floormodelloader.hpp
+++ include/graphics/scenegraph/modelloader/floormodelloader.hpp
@@ -2,6 +2,7 @@
 #define BB_FLOOR_MODEL_LOADER
 
 #include "graphics/scenegraph/modelloader/proceduralmodelloader.hpp"
+#include "models/infrastructure.hpp"
 #include "exceptions/genexc.hpp"
 #include <memory>
 
@@ -15,15 +16,15 @@ namespace BlueBear {
       class Model;
 
       namespace ModelLoader {
-
         class FloorModelLoader : public ProceduralModelLoader {
+          const std::vector< Models::Infrastructure::FloorLevel >& levels;
+          unsigned int currentLevel = 0;
 
-          std::shared_ptr< Model > generateTerrain();
+          sf::Image generateRootTexture( const glm::uvec2& dimensions );
 
         public:
-          EXCEPTION_TYPE( CollectionNotSetException, "Collection not set on FloorModelLoader" );
-
-          FloorModelLoader();
+          FloorModelLoader( const std::vector< Models::Infrastructure::FloorLevel >& levels );
+          void setCurrentLevel( unsigned int level );
 
           virtual std::shared_ptr< Model > get();
         };
--- src/graphics/scenegraph/modelloader/floormodelloader.cpp
+++ src/graphics/scenegraph/modelloader/floormodelloader.cpp
@@ -1,23 +1,43 @@
 #include "graphics/scenegraph/modelloader/floormodelloader.hpp"
 #include "graphics/scenegraph/model.hpp"
+#include <set>
 
 namespace BlueBear {
   namespace Graphics {
     namespace SceneGraph {
       namespace ModelLoader {
 
-        FloorModelLoader::FloorModelLoader() {}
+        FloorModelLoader::FloorModelLoader( const std::vector< Models::Infrastructure::FloorLevel >& levels ) :
+          levels( levels ) {}
 
-        std::shared_ptr< Model > FloorModelLoader::generateTerrain() {
+        void FloorModelLoader::setCurrentLevel( unsigned int level ) {
+          currentLevel = level;
+        }
 
+        sf::Image FloorModelLoader::generateRootTexture( const glm::uvec2& dimensions ) {
+          sf::Image image;
+
+          image.create( dimensions.x, dimensions.y );
+
+          return image;
         }
 
         std::shared_ptr< Model > FloorModelLoader::get() {
-          if( !floor ) {
-            throw CollectionNotSetException();
+          const Models::Infrastructure::FloorLevel& floorLevel = levels.at( currentLevel );
+          std::set< glm::vec3 > vertices;
+
+          // Create parent texture
+
+          for( int y = 0; y != floorLevel.dimensions.y - 1; y++ ) {
+            for( int x = 0; x != floorLevel.dimensions.x - 1; x++ ) {
+              if( floorLevel.tiles[ y ][ x ] ) {
+                // Floor tile is present, add this to the mesh
+                // Floor images should be 48x48 images
+              } else {
+                // Floor tile is not present
+              }
+            }
           }
-
-
         }
 
       }
