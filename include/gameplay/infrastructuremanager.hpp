4f5fc14de3ce0449e117b12bdbe1942803e89db7
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-02-17 18:02:22 -0500

    Starting new triangle-based brute force idea

--- include/gameplay/infrastructuremanager.hpp
+++ include/gameplay/infrastructuremanager.hpp
@@ -5,6 +5,7 @@
 #include "models/infrastructure.hpp"
 #include <jsoncpp/json/json.h>
 #include <glm/glm.hpp>
+#include <vector>
 
 namespace BlueBear::State { class HouseholdGameplayState; }
 namespace BlueBear::Gameplay {
@@ -12,6 +13,22 @@ namespace BlueBear::Gameplay {
 	class InfrastructureManager : public State::Substate {
 		Models::Infrastructure model;
 		std::vector< std::vector< glm::vec3 > > rooms;
+
+		struct RoomBuilderCell {
+			bool upperWall;
+			bool leftWall;
+			bool diagonalWall;
+			bool reverseDiagonalWall;
+			int upperColor = -1;
+			int rightColor = -1;
+			int lowerColor = -1;
+			int leftCorner = -1;
+		};
+
+		using RoomBuilderMap = std::vector< std::vector< RoomBuilderCell > >;
+
+		RoomBuilderMap prepareRoomBuilderMap( const glm::ivec2& dimensions );
+		void addLineSegment( glm::ivec2 start, glm::ivec2 end, RoomBuilderMap& map );
 
 	public:
 		InfrastructureManager( State::State& state );
