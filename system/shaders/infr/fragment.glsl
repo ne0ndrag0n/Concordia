9339791fd71e0a060ec3d5bc4299bdd793950706
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-03-03 12:05:14 -0500

    Starting work in fragment shader for point in polygon

--- system/shaders/infr/fragment.glsl
+++ system/shaders/infr/fragment.glsl
@@ -4,6 +4,9 @@ in vec3 fragNormal;
 in vec3 fragPos;
 out vec4 color;
 
+#define   YES    1.0f
+#define   NO     0.0f
+
 struct Material {
   sampler2D diffuse0;
   float shininess;
@@ -17,21 +20,66 @@ struct DirectionalLight {
   vec3 specular;
 };
 
-struct SectorDirectionalLight {
-  vec3 boxBottomLeft;
-  vec3 boxTopRight;
-  vec3 direction;
-  vec3 ambient;
-  vec3 diffuse;
-  vec3 specular;
+struct LineSegment {
+  vec2 from;
+  vec2 to;
+  float level;
+};
+
+struct LightSector {
+  DirectionalLight light;
+  int numSides;
+  int polygon[ 32 ];
 };
 
 uniform vec3 cameraPos;
 uniform Material material;
 uniform DirectionalLight directionalLight;
+uniform LineSegment lineSegments[ 128 ];
+uniform LightSector sectors[ 64 ];
 
-uniform uint numSectorDirectionalLights;
-uniform SectorDirectionalLight sectorDirectionalLights[ 128 ];
+float segmentsIntersect( LineSegment line1, LineSegment line2 ) {
+  // ta = (y3−y4)(x1−x3)+(x4−x3)(y1−y3)
+  //      -----------------------------
+  //      (x4−x3)(y1−y2)−(x1−x2)(y4−y3)
+
+  // tb = (y1−y2)(x1−x3)+(x2−x1)(y1−y3)
+  //      -----------------------------
+  //      (x4−x3)(y1−y2)−(x1−x2)(y4−y3)
+
+  // from - odd
+  // to - even
+
+  float denominator = ( ( line2.to.x - line2.from.x ) * ( line1.from.y - line1.to.y ) ) -
+                      ( ( line1.from.x - line1.to.x ) * ( line2.to.y - line2.from.y ) );
+
+  // collinear
+  if( denominator == 0.0f ) {
+    return NO;
+  }
+
+  // TODO: Check these with equation above
+  float ta_numerator = ( ( line2.from.y - line2.to.y ) * ( line1.from.x - line2.from.x ) ) +
+                       ( ( line2.to.x - line2.from.x ) * ( line1.from.y - line2.from.y ) );
+
+  float tb_numerator = ( ( line1.from.y - line1.to.y ) * ( line1.from.x - line2.from.x ) ) +
+                       ( ( line1.to.x - line1.from.x ) * ( line1.from.y - line2.from.y ) );
+
+
+  float ta = ta_numerator / denominator;
+  float tb = tb_numerator / denominator;
+
+  if( ta >= 0.0f && ta <= 1.0f && tb >= 0.0f && tb <= 1.0f ) {
+    return YES;
+  } else {
+    return NO;
+  }
+}
+
+float pointInPolygon( vec2 point ) {
+  // TODO
+  return 0.0f;
+}
 
 void main() {
   vec3 texResult = texture( material.diffuse0, fragTexture ).rgb;
