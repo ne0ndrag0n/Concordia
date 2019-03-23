aa9d60b3b0528861ffb8f8d0c3c9b8ce24e6f934
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-03-22 23:04:37 -0400

    trying overlay on scroller?

--- include/graphics/userinterface/widgets/scroll.hpp
+++ include/graphics/userinterface/widgets/scroll.hpp
@@ -11,11 +11,14 @@ namespace BlueBear::Graphics::Vector { class Renderer; }
 namespace BlueBear::Graphics::UserInterface::Widgets {
 
   class Scroll : public Element {
+    friend class ScrollOverlay;
+
     float scrollX = 0.0f;
     float scrollY = 0.0f;
-    bool forceDirty = false;
 
-
+    bool addedChildren();
+    bool getXScrollSet();
+    bool getYScrollSet();
     bool getXVisible();
     bool getYVisible();
     float getXRatio();
@@ -25,10 +28,11 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
     int getXSpace();
     int getYSpace();
 
+    std::shared_ptr< Element > getSingleContainer();
+
   protected:
     Scroll( const std::string& id, const std::vector< std::string >& classes );
 
-    void onMouseDown( Device::Input::Metadata event );
     void updateX( int x );
     void updateY( int y );
     glm::uvec2 getFinalRequisition( std::shared_ptr< Element > prospect );
@@ -36,13 +40,32 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
     void partialReflow();
 
   public:
-    virtual bool drawableDirty() override;
+    void setChildrenZOrder() override;
     virtual void positionAndSizeChildren() override;
-    virtual void render( Graphics::Vector::Renderer& renderer ) override;
     virtual void calculate() override;
     virtual void reflow( bool selectorsInvalidated = true ) override;
 
+    glm::vec2 getScrollAttributes();
+
     static std::shared_ptr< Scroll > create( const std::string& id, const std::vector< std::string >& classes );
+  };
+
+  // Redesign should have most of the logic encapsulated in ScrollOverlay
+  class ScrollOverlay : public Element {
+    bool forceDirty = false;
+
+  protected:
+    ScrollOverlay();
+    void onMouseDown( Device::Input::Metadata event );
+
+  public:
+    void regenerate();
+    void calculate() override;
+    bool drawableDirty() override;
+    void reflow( bool selectorsInvalidated = true ) override;
+    void render( Graphics::Vector::Renderer& renderer ) override;
+
+    static std::shared_ptr< ScrollOverlay > create();
   };
 
 }
--- src/graphics/userinterface/widgets/scroll.cpp
+++ src/graphics/userinterface/widgets/scroll.cpp
@@ -7,30 +7,44 @@
 
 namespace BlueBear::Graphics::UserInterface::Widgets {
 
-  Scroll::Scroll( const std::string& id, const std::vector< std::string >& classes ) : Element::Element( "Scroll", id, classes ) {
-    eventBundle.registerInputEvent( "mouse-down", std::bind( &Scroll::onMouseDown, this, std::placeholders::_1 ) );
-  }
+  Scroll::Scroll( const std::string& id, const std::vector< std::string >& classes ) : Element::Element( "Scroll", id, classes ) {}
 
   std::shared_ptr< Scroll > Scroll::create( const std::string& id, const std::vector< std::string >& classes ) {
     std::shared_ptr< Scroll > scroll( new Scroll( id, classes ) );
 
+    // Add shadow element
+    auto overlay = ScrollOverlay::create();
+    scroll->addChild( overlay, false );
+
     return scroll;
   }
 
+  bool Scroll::addedChildren() {
+    return children.size() >= 2;
+  }
+
+  bool Scroll::getXScrollSet() {
+    return localStyle.get< bool >( "scrollbar-x" );
+  }
+
+  bool Scroll::getYScrollSet() {
+    return localStyle.get< bool >( "scrollbar-y" );
+  }
+
   bool Scroll::getXVisible() {
-    return ( localStyle.get< bool >( "scrollbar-x" ) && getXRatio() < 1.0f );
+    return ( getXScrollSet() && getXRatio() < 1.0f );
   }
 
   bool Scroll::getYVisible() {
-    return ( localStyle.get< bool >( "scrollbar-y" ) && getYRatio() < 1.0f );
+    return ( getYScrollSet() && getYRatio() < 1.0f );
   }
 
   float Scroll::getXRatio() {
-    return ( ( float ) allocation[ 2 ] / ( float ) getFinalRequisition( children[ 0 ] ).x );
+    return ( ( float ) allocation[ 2 ] / ( float ) getFinalRequisition( getSingleContainer() ).x );
   }
 
   float Scroll::getYRatio() {
-    return ( ( float ) allocation[ 3 ] / ( float ) getFinalRequisition( children[ 0 ] ).y );
+    return ( ( float ) allocation[ 3 ] / ( float ) getFinalRequisition( getSingleContainer() ).y );
   }
 
   int Scroll::getXGutter() {
@@ -49,42 +63,12 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
     return getYGutter() - 2;
   }
 
-  void Scroll::onMouseDown( Device::Input::Metadata event ) {
-    auto relative = toRelative( event.mouseLocation );
-
-    if( getXVisible() ) {
-      if( relative.x >= 0 && relative.y >= allocation[ 3 ] - 10 && relative.x <= getXGutter() && relative.y <= allocation[ 3 ] ) {
-        updateX( relative.x );
-
-        manager->setupBlockingGlobalEvent( "mouse-moved", [ & ]( Device::Input::Metadata e ) {
-          updateX( toRelative( e.mouseLocation ).x );
-        } );
-
-        manager->setupBlockingGlobalEvent( "mouse-up", [ & ]( Device::Input::Metadata e ) {
-          manager->unregisterBlockingGlobalEvent( "mouse-moved" );
-          manager->unregisterBlockingGlobalEvent( "mouse-up" );
-        } );
-      }
-    }
-
-    if( getYVisible() ) {
-      if( relative.x >= allocation[ 2 ] - 10 && relative.y >= 0 && relative.x <= allocation[ 2 ] && relative.y <= getYGutter() ) {
-        updateY( relative.y );
-
-        manager->setupBlockingGlobalEvent( "mouse-moved", [ & ]( Device::Input::Metadata e ) {
-          updateY( toRelative( e.mouseLocation ).y );
-        } );
-
-        manager->setupBlockingGlobalEvent( "mouse-up", [ & ]( Device::Input::Metadata e ) {
-          manager->unregisterBlockingGlobalEvent( "mouse-moved" );
-          manager->unregisterBlockingGlobalEvent( "mouse-up" );
-        } );
-      }
-    }
+  std::shared_ptr< Element > Scroll::getSingleContainer() {
+    return children[ 1 ];
   }
 
   void Scroll::updateX( int x ) {
-    if( localStyle.get< bool >( "scrollbar-x" ) ) {
+    if( getXScrollSet() ) {
       int xSpace = getXSpace();
       int boxWidth = xSpace * getXRatio();
       int newX = x - ( boxWidth / 2 );
@@ -104,7 +88,7 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
   }
 
   void Scroll::updateY( int y ) {
-    if( localStyle.get< bool >( "scrollbar-y" ) ) {
+    if( getYScrollSet() ) {
       int ySpace = getYSpace();
       int boxWidth = ySpace * getYRatio();
       int newY = y - ( boxWidth / 2 );
@@ -127,8 +111,7 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
    */
   void Scroll::partialReflow() {
     positionAndSizeChildren();
-    forceDirty = true;
-    generateDrawable();
+    std::static_pointer_cast< ScrollOverlay >( children[ 0 ] )->regenerate();
   }
 
   glm::uvec2 Scroll::getFinalRequisition( std::shared_ptr< Element > prospect ) {
@@ -141,18 +124,11 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
     };
   }
 
-  bool Scroll::drawableDirty() {
-    if( forceDirty ) {
-      forceDirty = false;
-      return true;
-    }
-
-    return Element::drawableDirty();
-  }
-
   void Scroll::positionAndSizeChildren() {
-    if( children.size() ) {
-      std::shared_ptr< Element > onlyChild = children[ 0 ];
+    children[ 0 ]->setAllocation( allocation, false );
+
+    if( addedChildren() ) {
+      std::shared_ptr< Element > onlyChild = getSingleContainer();
       glm::uvec2 finalRequisition = getFinalRequisition( onlyChild );
 
       onlyChild->setAllocation( {
@@ -164,52 +140,19 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
     }
   }
 
-  void Scroll::render( Graphics::Vector::Renderer& renderer ) {
-    if( children.size() ) {
-      std::shared_ptr< Element > onlyChild = children[ 0 ];
-
-      if( getXVisible() ) {
-        // Gutter
-        renderer.drawRect(
-          { 0, allocation[ 3 ] - 10, getXGutter(), allocation[ 3 ] },
-          localStyle.get< glm::uvec4 >( "background-color" )
-        );
-
-        int xSpace = getXSpace();
-        int barSize = xSpace * getXRatio();
-
-        // Bar
-        renderer.drawRect(
-          { 1 + ( scrollX * xSpace ), allocation[ 3 ] - 9, 1 + ( scrollX * xSpace ) + barSize, allocation[ 3 ] - 1 },
-          localStyle.get< glm::uvec4 >( "color" )
-        );
-      }
-
-      if( getYVisible() ) {
-        // Gutter
-        renderer.drawRect(
-          { allocation[ 2 ] - 10, 0, allocation[ 2 ], getYGutter() },
-          localStyle.get< glm::uvec4 >( "background-color" )
-        );
-
-        int ySpace = getYSpace();
-        int barSize = ySpace * getYRatio();
-
-        // Bar
-        renderer.drawRect(
-          { allocation[ 2 ] - 9, 1 + ( scrollY * ySpace ), allocation[ 2 ] - 1, 1 + ( scrollY * ySpace ) + barSize },
-          localStyle.get< glm::uvec4 >( "color" )
-        );
-      }
+  void Scroll::setChildrenZOrder() {
+    children[ 0 ]->setLocalZOrder( 1 );
+    if( addedChildren() ) {
+      getSingleContainer()->setLocalZOrder( 2 );
     }
   }
 
   void Scroll::calculate() {
-    if( children.size() ) {
-      children[ 0 ]->calculate();
+    if( addedChildren() ) {
+      getSingleContainer()->calculate();
     }
 
-    glm::uvec2 differential{ localStyle.get< bool >( "scrollbar-x" ) ? 10 : 0, localStyle.get< bool >( "scrollbar-y" ) ? 10 : 0 };
+    glm::uvec2 differential{ getXScrollSet() ? 10 : 0, getYScrollSet() ? 10 : 0 };
     requisition = glm::uvec2{ 1, 1 } + differential;
   }
 
@@ -218,6 +161,128 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
       parent->reflow( selectorsInvalidated );
     } else {
       Element::reflow( selectorsInvalidated );
+    }
+  }
+
+  glm::vec2 Scroll::getScrollAttributes() {
+    return { scrollX, scrollY };
+  }
+
+  ScrollOverlay::ScrollOverlay() : Element::Element( "ScrollOverlay", "", { "-bb-shadow-scrolloverlay" } ) {
+    shadow = true;
+    eventBundle.registerInputEvent( "mouse-down", std::bind( &ScrollOverlay::onMouseDown, this, std::placeholders::_1 ) );
+  }
+
+  std::shared_ptr< ScrollOverlay > ScrollOverlay::create() {
+    return std::shared_ptr< ScrollOverlay >( new ScrollOverlay() );
+  }
+
+  void ScrollOverlay::onMouseDown( Device::Input::Metadata event ) {
+    auto parent = std::static_pointer_cast< Scroll >( getParent() );
+    auto relative = toRelative( event.mouseLocation );
+    auto allocation = parent->getAllocation();
+
+    if( parent->getXVisible() ) {
+      if( relative.x >= 0 && relative.y >= allocation[ 3 ] - 10 && relative.x <= parent->getXGutter() && relative.y <= allocation[ 3 ] ) {
+        parent->updateX( relative.x );
+
+        manager->setupBlockingGlobalEvent( "mouse-moved", [ parent ]( Device::Input::Metadata e ) {
+          parent->updateX( parent->toRelative( e.mouseLocation ).x );
+        } );
+
+        manager->setupBlockingGlobalEvent( "mouse-up", [ parent ]( Device::Input::Metadata e ) {
+          manager->unregisterBlockingGlobalEvent( "mouse-moved" );
+          manager->unregisterBlockingGlobalEvent( "mouse-up" );
+        } );
+      }
+    }
+
+    if( parent->getYVisible() ) {
+      if( relative.x >= allocation[ 2 ] - 10 && relative.y >= 0 && relative.x <= allocation[ 2 ] && relative.y <= parent->getYGutter() ) {
+        parent->updateY( relative.y );
+
+        manager->setupBlockingGlobalEvent( "mouse-moved", [ parent ]( Device::Input::Metadata e ) {
+          parent->updateY( parent->toRelative( e.mouseLocation ).y );
+        } );
+
+        manager->setupBlockingGlobalEvent( "mouse-up", [ parent ]( Device::Input::Metadata e ) {
+          manager->unregisterBlockingGlobalEvent( "mouse-moved" );
+          manager->unregisterBlockingGlobalEvent( "mouse-up" );
+        } );
+      }
+    }
+  }
+
+  void ScrollOverlay::regenerate() {
+    forceDirty = true;
+    generateDrawable();
+  }
+
+  void ScrollOverlay::calculate() {
+    // Nothing to calculate - height and width are known when positioned
+    requisition = { 0, 0 };
+  }
+
+  bool ScrollOverlay::drawableDirty() {
+    if( forceDirty ) {
+      forceDirty = false;
+      return true;
+    }
+
+    return Element::drawableDirty();
+  }
+
+  void ScrollOverlay::reflow( bool selectorsInvalidated ) {
+    if( auto parent = getParent() ) {
+      parent->reflow( selectorsInvalidated );
+    } else {
+      Element::reflow( selectorsInvalidated );
+    }
+  }
+
+  void ScrollOverlay::render( Graphics::Vector::Renderer& renderer ) {
+    auto parent = std::static_pointer_cast< Scroll >( getParent() );
+
+    auto attributes = parent->getScrollAttributes();
+    float scrollX = attributes.x;
+    float scrollY = attributes.y;
+
+    auto allocation = parent->getAllocation();
+
+    if( parent->addedChildren() ) {
+      if( parent->getXVisible() ) {
+        // Gutter
+        renderer.drawRect(
+          { 0, allocation[ 3 ] - 10, parent->getXGutter(), allocation[ 3 ] },
+          parent->getPropertyList().get< glm::uvec4 >( "background-color" )
+        );
+
+        int xSpace = parent->getXSpace();
+        int barSize = xSpace * parent->getXRatio();
+
+        // Bar
+        renderer.drawRect(
+          { 1 + ( scrollX * xSpace ), allocation[ 3 ] - 9, 1 + ( scrollX * xSpace ) + barSize, allocation[ 3 ] - 1 },
+          parent->getPropertyList().get< glm::uvec4 >( "color" )
+        );
+      }
+
+      if( parent->getYVisible() ) {
+        // Gutter
+        renderer.drawRect(
+          { allocation[ 2 ] - 10, 0, allocation[ 2 ], parent->getYGutter() },
+          parent->getPropertyList().get< glm::uvec4 >( "background-color" )
+        );
+
+        int ySpace = parent->getYSpace();
+        int barSize = ySpace * parent->getYRatio();
+
+        // Bar
+        renderer.drawRect(
+          { allocation[ 2 ] - 9, 1 + ( scrollY * ySpace ), allocation[ 2 ] - 1, 1 + ( scrollY * ySpace ) + barSize },
+          parent->getPropertyList().get< glm::uvec4 >( "color" )
+        );
+      }
     }
   }
 
