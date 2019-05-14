f7017c0fb4b0f5792c5b53c90078bf1b43ed0f36
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-05-14 17:54:14 -0400

    Updates to shader for new room method

--- system/shaders/common/cond.glsl
+++ system/shaders/common/cond.glsl
@@ -1,3 +1,19 @@
-float equalSwitch( float x, float y ) {
+float whenEqual( float x, float y ) {
 	return 1.0 - abs( sign( x - y ) );
+}
+
+float whenLessThan( float x, float y ) {
+	return max( sign( y - x ), 0.0 );
+}
+
+float whenGreaterThan( float x, float y ) {
+	return max( sign( y - x ), 0.0 );
+}
+
+float whenGreaterEqual( float x, float y ) {
+  return 1.0 - whenLessThan( x, y );
+}
+
+float whenLessEqual( float x, float y ) {
+  return 1.0 - whenGreaterThan( x, y );
 }
--- system/shaders/common/room.glsl
+++ system/shaders/common/room.glsl
@@ -7,45 +7,66 @@ struct Room {
 	vec2 lowerLeft;
 	vec2 upperRight;
 	uint level;
-	uint roomId; // object is invalid if this is 0
+	uint samplerId; // object is invalid if this is 0
 };
 
 uniform Room rooms[ MAX_ROOMS ];
-
 uniform int availableRooms;
 
 uniform sampler2D sector0;
 uniform sampler2D sector1;
 uniform sampler2D sector2;
 uniform sampler2D sector3;
-uniform sampler2D sector4;
-uniform sampler2D sector5;
-uniform sampler2D sector6;
-uniform sampler2D sector7;
-uniform sampler2D sector8;
-uniform sampler2D sector9;
-uniform sampler2D sector10;
-uniform sampler2D sector11;
-uniform sampler2D sector12;
-uniform sampler2D sector13;
-uniform sampler2D sector14;
-uniform sampler2D sector15;
+// TODO
 
 float fragmentInBox( const Room room, const vec2 fragment ) {
 	vec2 stp = step( room.lowerLeft, fragment ) - step( room.upperRight, fragment );
 	return stp.x * stp.y;
 }
 
-DirectionalLight getRoomLight( const vec3 fragment ) {
-	float level = clamp( fragment.z, 0.0f, 3.402823466e+38 ) / 4.0f;
+float lookupFragment( const Room room, const vec2 fragment ) {
+	vec2 textureCoordinates = ( fragment - room.lowerLeft ) / ( room.upperRight - room.lowerLeft );
 
-	// step( needle, value ) - if value >= needle, return 1
 	float lightIndex = 0.0f;
-	for( int i = 0; i != MAX_ROOMS; i++ ) {
-		float prevNonzero = step( 1.0f, lightIndex );
-		float roomIndex = fragmentInBox( rooms[ i ], fragment.xy ) * equalSwitch( float( rooms[ i ].level ), level ) * float( rooms[ i ].roomId );
-		lightIndex = ( prevNonzero * lightIndex ) + ( ( 1 - prevNonzero ) * roomIndex );
+
+	// does this break the universe?
+	switch( room.samplerId ) {
+		default:
+		case 0:
+			lightIndex = texture( sector0, textureCoordinates ).r;
+			break;
+		case 1:
+			lightIndex = texture( sector1, textureCoordinates ).r;
+			break;
+		case 2:
+			lightIndex = texture( sector2, textureCoordinates ).r;
+			break;
+		case 3:
+			lightIndex = texture( sector3, textureCoordinates ).r;
+			break;
+		// TODO
 	}
 
-	return directionalLights[ lightIndex ];
+	return
+		whenGreaterEqual( textureCoordinates.x, 0.0f ) *
+		whenGreaterEqual( textureCoordinates.y, 0.0f ) *
+		whenLessEqual( textureCoordinates.x, 1.0f ) *
+		whenLessEqual( textureCoordinates.y, 1.0f ) *
+		lightIndex;
+}
+
+DirectionalLight getRoomLight( const vec3 fragment ) {
+	// Clamp z to all positive values
+	float level = clamp( fragment.z, 0.0f, 3.402823466e+38 ) / 4.0f;
+
+	float lightIndex = 0.0f;
+	for( int i = 0; i != MAX_ROOMS; i++ ) {
+		lightIndex =
+			whenEqual( lightIndex, 0.0f ) *							// lightIndex must not have been set before
+			fragmentInBox( rooms[ i ], fragment.xy ) *				// fragPos intersects room bounding box
+			whenEqual( float( rooms[ i ].level ), level ) *			// room is on the same level as this fragment
+			lookupFragment( rooms[ i ], fragment.xy );				// Lookup conducted on fragment returns actual index
+	}
+
+	return directionalLights[ lightIndex ];							// If we truly don't hit on a sector, return the outdoor light at position 0
 }
