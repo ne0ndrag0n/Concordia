e523058a8dac92e46bc514f1de08c1a2617f7218
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2019-06-13 19:05:55 -0400

    Fix requisition bug

--- src/graphics/userinterface/widgets/fixed_layout.cpp
+++ src/graphics/userinterface/widgets/fixed_layout.cpp
@@ -14,29 +14,18 @@ namespace BlueBear::Graphics::UserInterface::Widgets {
 	}
 
 	void FixedLayout::positionAndSizeChildren() {
-          if( getParent() == nullptr ) {
-            calculate();
-          }
+		if( getParent() == nullptr ) {
+			calculate();
+		}
 
 		for( auto& child : children ) {
-			int width = child->getPropertyList().get< int >( "width" );
-			int height = child->getPropertyList().get< int >( "height" );
-
-			if( !Utility::valueIsLiteral( width ) ) {
-				Log::getInstance().warn( "FixedLayout::positionAndSizeChildren", "Non-literal value given for width; defaulting to 10" );
-				width = 10;
-			}
-
-			if( !Utility::valueIsLiteral( height ) ) {
-				Log::getInstance().warn( "FixedLayout::positionAndSizeChildren", "Non-literal value given for height; defaulting to 10" );
-				height = 10;
-			}
+			glm::uvec2 finalRequisition = Utility::getFinalRequisition( child );
 
 			child->setAllocation( {
 				child->getPropertyList().get< int >( "left" ),
 				child->getPropertyList().get< int >( "top" ),
-				width,
-				height
+				finalRequisition.x,
+				finalRequisition.y
 			}, false );
 		}
 	}
